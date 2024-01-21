7c7a8d5d2f22aed7bb6f6638cea667d6013dca82
Nwatu Martin Chukwuemeka <128225275+martinmatics100@users.noreply.github.com>
2024-01-21 19:13:07 +0100

    my mediconnect api project commit

--- MediConnect.Api/MediConnect.Core/Dto/ApiResponse.cs
+++ MediConnect.Api/MediConnect.Core/Dto/ApiResponse.cs
@@ -8,5 +8,40 @@ namespace MediConnect.Core.Dto
 {
     public class ApiResponse
     {
+        public bool Succeseded { get; set; }
+        public string Message { get; set; }
+        public List<string> Errors { get; set; }
+        public object Data { get; set; }
+
+        public ApiResponse( string message = null)
+        {
+            Succeseded = true;
+            Message = message;
+        }
+
+        public static ApiResponse Success (object data)
+        {
+            return new ApiResponse
+            {
+                Message = "success",
+                Data = data
+            };
+        }
+
+        public static ApiResponse Failed (object data, string Message = "Failure", List<string> errors = null)
+        {
+            return new ApiResponse
+            {
+                Succeseded = false,
+                Data = data,
+                Message = Message,
+                Errors = errors
+            };
+        }
+    }
+
+    public class ApiResponse<T> : ApiResponse
+    {
+        public T Data { get; set; }
     }
 }
