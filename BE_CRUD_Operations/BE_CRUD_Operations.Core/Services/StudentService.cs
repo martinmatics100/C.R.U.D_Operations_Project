bd10a243c37bb0c91e39b73fc42b43d456c48e42
Nwatu Martin Chukwuemeka <128225275+martinmatics100@users.noreply.github.com>
2024-03-27 22:43:51 +0100

    commit all

--- BE_CRUD_Operations/BE_CRUD_Operations.Core/Services/StudentService.cs
+++ BE_CRUD_Operations/BE_CRUD_Operations.Core/Services/StudentService.cs
@@ -32,7 +32,19 @@ namespace BE_CRUD_Operations.Core.Implementation
 
                 var student = _mapper.Map<Student>(studentDTO);
 
-                if(student == null)
+                //// Create a new Student instance
+                //var student = new Student();
+
+                //// Map properties from StudentDTO to Student manually
+                //student.FirstName = studentDTO.FirstName;
+                //student.LastName = studentDTO.LastName;
+                //student.Department = studentDTO.Department;
+                //student.Gender = studentDTO.Gender;
+                //student.DateOfBirth = studentDTO.DateOfBirth;
+                //student.Age = studentDTO.Age;
+                //student.IsGraduated = studentDTO.IsGraduated;
+
+                if (student == null)
                 {
                     return false;
                 }
@@ -73,6 +85,32 @@ namespace BE_CRUD_Operations.Core.Implementation
         {
             var students = await _baseRepository.GetAllAsync();
             return _mapper.Map<IEnumerable<StudentDTO>>(students);
+
+            //// Create a list to store mapped StudentDTO objects
+            //var studentDTOs = new List<StudentDTO>();
+
+            //// Iterate through each student and map it to StudentDTO manually
+            //foreach (var student in students)
+            //{
+            //    // Create a new StudentDTO instance
+            //    var studentDTO = new StudentDTO();
+
+            //    // Map properties from Student to StudentDTO manually
+            //    studentDTO.FirstName = student.FirstName;
+            //    studentDTO.LastName = student.LastName;
+            //    studentDTO.Department = student.Department;
+            //    studentDTO.Gender = student.Gender;
+            //    studentDTO.DateOfBirth = student.DateOfBirth;
+            //    studentDTO.Age = student.Age;
+            //    studentDTO.IsGraduated = student.IsGraduated;
+
+            //    // Add the mapped StudentDTO to the list
+            //    studentDTOs.Add(studentDTO);
+            //}
+
+            //// Return the list of mapped StudentDTOs
+            //return studentDTOs;
+
         }
 
         public async Task<StudentDTO> GetStudentById(string studentId)
@@ -85,6 +123,21 @@ namespace BE_CRUD_Operations.Core.Implementation
                     return null;
 
                 return _mapper.Map<StudentDTO>(gottenStudent);
+
+                //// Create a new StudentDTO instance
+                //var studentDTO = new StudentDTO();
+
+                //// Map properties from gottenStudent to StudentDTO manually
+                //studentDTO.FirstName = gottenStudent.FirstName;
+                //studentDTO.LastName = gottenStudent.LastName;
+                //studentDTO.Department = gottenStudent.Department;
+                //studentDTO.Gender = gottenStudent.Gender;
+                //studentDTO.DateOfBirth = gottenStudent.DateOfBirth;
+                //studentDTO.Age = gottenStudent.Age;
+                //studentDTO.IsGraduated = gottenStudent.IsGraduated;
+
+                //// Return the mapped StudentDTO
+                //return studentDTO;
             }
             catch(Exception ex)
             {
@@ -105,6 +158,16 @@ namespace BE_CRUD_Operations.Core.Implementation
                 }
 
                 _mapper.Map(updatedStudentDTO, existingStudent);
+
+                //// Update the properties of the existing student with the values from updatedStudentDTO manually
+                //existingStudent.FirstName = updatedStudentDTO.FirstName;
+                //existingStudent.LastName = updatedStudentDTO.LastName;
+                //existingStudent.Department = updatedStudentDTO.Department;
+                //existingStudent.Gender = updatedStudentDTO.Gender;
+                //existingStudent.DateOfBirth = updatedStudentDTO.DateOfBirth;
+                //existingStudent.Age = updatedStudentDTO.Age;
+                //existingStudent.IsGraduated = updatedStudentDTO.IsGraduated;
+
                 await _baseRepository.UpdateAsync(existingStudent);
 
                 return true;
---
+++ BE_CRUD_Operations/BE_CRUD_Operations.Data/obj/Debug/net7.0/owaulpa3.sql
@@ -0,0 +1,60 @@
+﻿BEGIN TRANSACTION;
+GO
+
+EXEC sp_rename N'[students].[StudentName]', N'LastName', N'COLUMN';
+GO
+
+EXEC sp_rename N'[students].[StudentCourse]', N'Department', N'COLUMN';
+GO
+
+ALTER TABLE [students] ADD [Age] int NOT NULL DEFAULT 0;
+GO
+
+ALTER TABLE [students] ADD [DateOfBirth] datetime2 NOT NULL DEFAULT '0001-01-01T00:00:00.0000000';
+GO
+
+ALTER TABLE [students] ADD [FirstName] nvarchar(20) NOT NULL DEFAULT N'';
+GO
+
+ALTER TABLE [students] ADD [Gender] int NOT NULL DEFAULT 0;
+GO
+
+ALTER TABLE [students] ADD [IsGraduated] bit NOT NULL DEFAULT CAST(0 AS bit);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20240318165213_updated-StudentModel', N'7.0.10');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20240318165725_removed-Max-attribute', N'7.0.10');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+DECLARE @var0 sysname;
+SELECT @var0 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[students]') AND [c].[name] = N'DateOfBirth');
+IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [students] DROP CONSTRAINT [' + @var0 + '];');
+ALTER TABLE [students] ALTER COLUMN [DateOfBirth] Date NOT NULL;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20240325192053_column=Date-Added', N'7.0.10');
+GO
+
+COMMIT;
+GO
+
